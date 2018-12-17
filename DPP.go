ae316a5cd36641be9035c0af48401dcade1b5d88
Joseph S. Lipinski <jsl94@drexel.edu>
2018-12-17 18:55:55 -0500

    Moved philosopher creation, Current bug in obtaining forks. Needs debugging

--- DPP.go
+++ DPP.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 //Philosopher is a struct used to define all the attributes of a philosopher
@@ -18,6 +20,8 @@ type Philosopher struct {
 
 var wg sync.WaitGroup
 var forks = [10]chan bool{}
+var philosophers = [10]Philosopher{}
+
 var philosopherNames = []string{
 	"Aristotle",
 	"Immanuel Kant",
@@ -29,7 +33,131 @@ var philosopherNames = []string{
 	"Sun Tzu",
 	"Socrates",
 	"Siddhartha Gautama Buddha"}
-var philosophers = [10]Philosopher{}
+
+//SayHello is a simple function that makes philosophers state their name
+func SayHello(_philosopher *Philosopher) {
+	fmt.Printf("Hello my name is %s\n", _philosopher.name)
+}
+
+//Think is a function that causes philosophers to think for some amount of time
+func Think(_philosopher *Philosopher) {
+	fmt.Printf("I, %s, am thinking\n", _philosopher.name)
+	time.Sleep(time.Duration(1+rand.Intn(10)) * time.Second)
+	fmt.Printf("I, %s, am done thinking\n", _philosopher.name)
+}
+
+//GetLeftFork is a function that causes philosopher's to obtain a fork
+func GetLeftFork(_philosopher *Philosopher) {
+	fmt.Printf("I, %s, need a fork\n", _philosopher.name)
+	forkAcquired := false
+	for {
+		if !forkAcquired {
+			select {
+			case status := <-_philosopher.leftFork:
+				if status {
+					_philosopher.leftFork <- false
+					forkAcquired = true
+				} else {
+					_philosopher.leftFork <- false
+					fmt.Printf("I, %s, still need a fork\n", _philosopher.name)
+				}
+			default:
+				time.Sleep(time.Second * 2)
+				fmt.Printf("I, %s, still need a fork\n", _philosopher.name)
+			}
+		} else {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+//GetRightFork is a function that causes philosopher's to obtain a fork
+func GetRightFork(_philosopher *Philosopher) {
+	fmt.Printf("I, %s, need a fork\n", _philosopher.name)
+	forkAcquired := false
+	for {
+		if !forkAcquired {
+			select {
+			case status := <-_philosopher.rightFork:
+				if status {
+					_philosopher.rightFork <- false
+					forkAcquired = true
+				} else {
+					_philosopher.rightFork <- false
+					fmt.Printf("I, %s, still need a fork\n", _philosopher.name)
+				}
+			default:
+				time.Sleep(time.Second * 2)
+				fmt.Printf("I, %s, still need a fork\n", _philosopher.name)
+			}
+		} else {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+//Eat is a function that causes philosopher's to eat
+func Eat(_philosopher *Philosopher) {
+	fmt.Printf("I, %s, have two forks and think that theses noodles are delicious\n", _philosopher.name)
+}
+
+//PutForksDown is a functions that causes philosophers to put their forks back
+func PutForksDown(_philosopher *Philosopher) {
+	fork1, fork2 := false, false
+	for {
+		if !fork1 {
+			select {
+			case status := <-_philosopher.leftFork:
+				if !status {
+					_philosopher.leftFork <- true
+					fork1 = true
+				} else {
+					_philosopher.leftFork <- false
+				}
+			default:
+				_philosopher.leftFork <- true
+			}
+		} else {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	for {
+		if !fork2 {
+			select {
+			case status := <-_philosopher.rightFork:
+				if !status {
+					_philosopher.rightFork <- true
+					fork2 = true
+				} else {
+					_philosopher.rightFork <- false
+				}
+			default:
+				_philosopher.rightFork <- true
+
+			}
+		} else {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+//Dine is a function that handles all the behaviors of philosophers
+func Dine(_philosopher *Philosopher) {
+	SayHello(_philosopher)
+	Think(_philosopher)
+	GetLeftFork(_philosopher)
+	fmt.Println("I got a fork")
+	GetRightFork(_philosopher)
+	fmt.Println("I got a fork")
+	Eat(_philosopher)
+	PutForksDown(_philosopher)
+	fmt.Println("ALL DONE")
+	defer wg.Done()
+}
 
 //MakeFork produces a channel of type boolean and adds it to the forks array
 func MakeFork(index int) {
@@ -38,16 +166,37 @@ func MakeFork(index int) {
 	forks[index] = forkChannel
 }
 
-func SayHello(_philosopher *Philosopher) {
-	fmt.Printf("Hello my name is %s\n", _philosopher.name)
+func MakePhilosopher(_numberOfPhilosophers int) {
+	for i := 0; i < _numberOfPhilosophers; i++ {
+		_philosopher := &Philosopher{philosopherNames[i], nil, nil, nil, nil}
+		philosophers[i] = *_philosopher
+	}
 }
 
-func Dine(_philosopher *Philosopher) {
+func SetupLeftSide(_numberOfPhilosophers int) {
+	for i := 1; i < _numberOfPhilosophers-1; i++ {
+		philosophers[i].leftNeighbor = &philosophers[i-1]
 
-	//Think
-	//GetForks
-	//Dine
-	//wg.Done()
+	}
+	philosophers[0].leftNeighbor = &philosophers[_numberOfPhilosophers-1]
+
+	for i := 0; i < _numberOfPhilosophers-2; i++ {
+		philosophers[i].leftFork = forks[i]
+	}
+	philosophers[_numberOfPhilosophers-1].leftFork = forks[0]
+}
+
+func SetUpRightSide(_numberOfPhilosophers int) {
+	for i := 0; i < _numberOfPhilosophers-2; i++ {
+		philosophers[i].rightNeighbor = &philosophers[i-1]
+
+	}
+	philosophers[_numberOfPhilosophers-1].rightNeighbor = &philosophers[0]
+
+	for i := 0; i < _numberOfPhilosophers-2; i++ {
+		philosophers[i].rightFork = forks[i+1]
+	}
+	philosophers[_numberOfPhilosophers-1].rightFork = forks[_numberOfPhilosophers-1]
 }
 
 func main() {
@@ -60,7 +209,7 @@ func main() {
 		fmt.Println("An error has occurred based on the number that you have entered. The defualt value of two will be used")
 		numberOfPhilosophers = 2
 	}
-	if numberOfPhilosophers < 1 {
+	if numberOfPhilosophers < 2 {
 		fmt.Printf("You have selected an insufficent number to simulate. The defualt number of philosophers will be used instead")
 		numberOfPhilosophers = 2
 	} else if numberOfPhilosophers > 10 {
@@ -72,31 +221,13 @@ func main() {
 		MakeFork(i)
 	}
 
-	for i := 0; i < numberOfPhilosophers; i++ {
-		if i > 0 {
-			_philosopher := &Philosopher{philosopherNames[i], &philosophers[i-1], nil, forks[numberOfPhilosophers-1], nil}
-			philosophers[i] = *_philosopher
-		} else {
-			_philosopher := &Philosopher{philosopherNames[i], nil, nil, forks[i], nil}
-			philosophers[i] = *_philosopher
-		}
-	}
+	MakePhilosopher(numberOfPhilosophers)
+	SetupLeftSide(numberOfPhilosophers)
+	SetUpRightSide(numberOfPhilosophers)
 
-	philosophers[0].leftNeighbor = &philosophers[numberOfPhilosophers-1]
-
-	for i := numberOfPhilosophers - 1; i > 0; i-- {
-		if i == numberOfPhilosophers-1 {
-			philosophers[i].rightNeighbor = &philosophers[0]
-			philosophers[i].rightFork = forks[0]
-		} else {
-			philosophers[i].rightNeighbor = &philosophers[i+1]
-			philosophers[i].rightFork = forks[i+1]
-		}
-	}
 	for i := 0; i < numberOfPhilosophers; i++ {
-		SayHello(&philosophers[i])
-		Dine(&philosophers[i])
-		//wg.Add(1)
+		go Dine(&philosophers[i])
+		wg.Add(1)
 	}
 	/**/
 	//time.Sleep(time.Second)
